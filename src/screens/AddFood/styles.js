75b107f709d3782d97aad6a3885c0633c0e196bf
silfiL <silfilangie04@gmail.com>
2019-04-25 16:39:22 +0700

    Install 2 libs Modalbox and swiper, not tested

---
+++ src/screens/AddFood/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'raeact-native';
+
+export const styles = StyleSheet.create({
+    modal: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modal3: {
+        height: 300,
+        width: 300
+    },
+})
