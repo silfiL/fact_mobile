5192ac4cf601873f7bfec760d181101b7c5ad514
Silfi Langie <silfilangie04@gmail.com>
2019-07-17 08:05:41 +0700

    Added image for IntroSlider

--- src/screens/IntroSlider/IntroSlider.js
+++ src/screens/IntroSlider/IntroSlider.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Icon from 'react-native-vector-icons/AntDesign';
 import LinearGradient from 'react-native-linear-gradient';
-import { View, Text, AsyncStorage } from 'react-native';
+import { View, Text, AsyncStorage, Image } from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
 
 import { styles } from './styles';
@@ -13,28 +13,28 @@ const slides = [
     key: '1',
     title: 'For all your goals',
     text: 'A personalized app to help you lose, maintain or gain weight with monthly evaluation.',
-    icon: 'md-checkmark',
+    image: require('../../assets/img/3.png'),
     colors: [Color.GREEN,Color.LIGHT_GREEN],
   },
   {
     key: '2',
     title: 'Knowing is half the battle',
     text: 'Help to calculate your daily calories needed and provide with weekly & monthly statistics.',
-    icon: 'md-checkmark',
+    image: require('../../assets/img/2.png'),
     colors: [Color.BLUE,Color.LIGHT_BLUE],
   },
   {
     key: '3',
     title: 'Know what you eat',
     text: 'Easily track your calorie and nutrient intake based on our provided food database.',
-    icon: 'md-checkmark',
+    image: require('../../assets/img/1.png'),
     colors: [Color.YELLOW,Color.LIGHT_YELLOW],
   },
   {
     key: '4',
     title: 'Also know what you do',
     text: 'Lastly, help to track your activity and the calories burnt based on Human Activity Recognition.',
-    icon: 'md-checkmark',
+    image: require('../../assets/img/4.png'),
     colors: [Color.RED,Color.LIGHT_RED],
   }
 ];
@@ -74,7 +74,7 @@ export default class IntroSlider extends React.Component {
       }]}
       colors={props.colors}
       start={{x: 0, y: .1}} end={{x: .1, y: 1}}>
-      <Ionicons style={{ backgroundColor: 'transparent' }} name={props.icon} size={200} color={Color.APP_WHITE} />
+      <Image source={props.image} style={styles.image} resizeMode="contain"/>
       <View>
         <Text style={styles.title}>{props.title}</Text>
         <Text style={styles.text}>{props.text}</Text>
